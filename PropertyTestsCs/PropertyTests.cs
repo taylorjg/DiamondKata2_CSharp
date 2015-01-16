41cdd96d1a19ba9d2fae4b91cd5096efcd267cf4
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-01-16 09:42:30 +0000

    1 basic property specified in 4 different ways

--- PropertyTestsCs/PropertyTests.cs
+++ PropertyTestsCs/PropertyTests.cs
@@ -8,27 +8,20 @@ using FsCheck.NUnit;
 
 namespace PropertyTestsCs
 {
+    using Property =  Gen<Rose<Result>>;
+
     [NUnit.Framework.TestFixture]
     public class PropertyTests
     {
-        [Property(Verbose = true, MaxTest = 5)]
-        public bool Property1(char c)
-        {
-            return Diamond.GenerateLines(c).Any();
-        }
+        private static readonly Func<char, bool> BasicProperty = c => Diamond.GenerateLines(c).Any();
 
-        [Property(Verbose = true, MaxTest = 5)]
-        public Gen<Rose<Result>> Property2()
-        {
-            var genUpperCaseAlphaChar = Any.ValueIn(Enumerable.Range('A', 26).Select(Convert.ToChar));
-            return Spec.For(genUpperCaseAlphaChar, c => Diamond.GenerateLines(c).Any()).Build();
-        }
+        private static readonly Gen<char> GenUpperCaseAlphaChar = Any.ValueIn(Enumerable.Range('A', 26).Select(Convert.ToChar));
 
         private class UpperCaseAlphaCharArbitrary : Arbitrary<char>
         {
             public override Gen<char> Generator
             {
-                get { return Any.ValueIn(Enumerable.Range('A', 26).Select(Convert.ToChar)); }
+                get { return GenUpperCaseAlphaChar; }
             }
         }
 
@@ -41,10 +34,33 @@ namespace PropertyTestsCs
             }
         }
 
-        [Property(Verbose = true, MaxTest = 5, Arbitrary = new[] { typeof(MyArbitraries) })]
-        public bool Property3(char c)
+        [Property(Verbose = true, MaxTest = 5)]
+        public bool Property1(char c)
         {
-            return Diamond.GenerateLines(c).Any();
+            return BasicProperty(c);
+        }
+
+        [Property(Verbose = true, MaxTest = 5, Arbitrary = new[] { typeof(MyArbitraries) })]
+        public bool Property2(char c)
+        {
+            return BasicProperty(c);
+        }
+
+        [Property(Verbose = true, MaxTest = 5)]
+        public Property Property3()
+        {
+            return Spec
+                .For(GenUpperCaseAlphaChar, BasicProperty)
+                .Build();
+        }
+
+        [Property(Verbose = true, MaxTest = 5)]
+        public Property Property4()
+        {
+            Arb.register<MyArbitraries>();
+            return Spec
+                .ForAny(BasicProperty)
+                .Build();
         }
     }
 }
